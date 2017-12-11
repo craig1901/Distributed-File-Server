28cc559a40406ce3d0ee2ceb11d9305af9947258
Craig Nolan <nolancr@tcd.ie>
2017-12-11 18:54:09 +0000

    Created DB pool and perform migrations

--- DirectoryService/app/Main.hs
+++ DirectoryService/app/Main.hs
@@ -6,15 +6,23 @@
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE QuasiQuotes                #-}
 {-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeOperators              #-}
 {-# LANGUAGE TypeFamilies               #-}
 
 module Main where
 
 import  Control.Monad.IO.Class  (liftIO)
-import  Control.Monad.Logger    (runStderrLoggingT)
+import  Control.Monad.Logger    (runStderrLoggingT, runStdoutLoggingT, runNoLoggingT)
 import  Database.Persist
-import  Database.Persist.MySQL
+import  Database.Persist.MySQL  (ConnectionPool, MySQLConnectInfo, createMySQLPool, mkMySQLConnectInfo)
+import  Database.Persist.Sql
 import  Database.Persist.TH
+import System.Environment       (lookupEnv)
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 File
@@ -23,13 +31,21 @@ File
     deriving Show
 |]
 
-connectionInfo = defaultConnectInfo { connectHost = "localhost",
-                                      connectUser = "root",
-                                      connectPort = 3306,
-                                      connectPassword = "root",
-                                      connectDatabase = "Files"}
+conn :: IO MySQLConnectInfo
+conn = return (mkMySQLConnectInfo "localhost" "root" "root" "Files")
+
+makePool :: IO ConnectionPool
+makePool = do
+    c <- conn
+    runStdoutLoggingT (createMySQLPool c 1)
+
 
 main :: IO ()
-main = runStderrLoggingT $ withMySQLPool connectionInfo 10 $ \pool -> liftIO $ do
-         flip runSqlPersistMPool pool $ do
-           runMigration migrateAll
+main = do
+    p <- makePool
+    runSqlPool (runMigration migrateAll) p
+
+
+-- main = runStderrLoggingT $ withMySQLPool connectionInfo 10 $ \pool -> liftIO $ do
+--          flip runSqlPersistMPool pool $ do
+--            runMigration migrateAll
